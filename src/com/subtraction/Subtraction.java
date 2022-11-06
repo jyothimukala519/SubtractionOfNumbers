68095ff1042ce3e07827ceb8499f96c3401785fb
jyothimukala519 <117594929+jyothimukala519@users.noreply.github.com>
2022-11-07 00:46:35 +0530

    Subtraction of Numbers
    Subtraction of two numbers

---
+++ src/com/subtraction/Subtraction.java
@@ -0,0 +1,11 @@
+package com.subtraction;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		int a=40;
+		int b=20;
+		int c=a-b;
+		System.out.println("The Subtraction of Two numbers is "+c);
+	}
+}
